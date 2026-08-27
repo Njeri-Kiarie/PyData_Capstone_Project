56daad5240d3413f96f8b0a404130078cfb1b59b
Njeri <njerikiarie99@gmail.com>
2026-08-27 10:49:18 +0300

    update file

--- customer_churn_dashboard.py
+++ customer_churn_dashboard.py
@@ -361,7 +361,7 @@ with menu1:
     fig.update_layout(
         xaxis_title="Age Group",
         yaxis_title="Churn Rate (%)",
-        yaxis_range=[0, 60]
+        yaxis_range=[0, 30]
     )
 
     st.plotly_chart(
